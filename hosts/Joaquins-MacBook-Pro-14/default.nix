a78a0ecfae276ea66d8f3dec3bc80f35dbc72ed8
Joaquin Gatica <joaquin@anagram.dev>
2024-03-19 17:53:19 -0300

    feat: add additional configuration from template

--- hosts/Joaquins-MacBook-Pro-14/default.nix
+++ hosts/Joaquins-MacBook-Pro-14/default.nix
@@ -1,9 +1,11 @@
 {pkgs, ...}: {
-  services.nix-daemon.enable = true;
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =[
+    # pkgs.vim
+  ];
 
   nix = {
-    settings.trusted-users = [ "root" "joaquin" ];
-
     linux-builder = {
       enable = false;
       maxJobs = 4;
@@ -11,5 +13,32 @@
         virtualisation.cores = 4;
       };
     };
+
+    settings = {
+      # Necessary for using flakes on this system.
+      experimental-features = "nix-command flakes";
+      trusted-users = [ "root" "joaquin" ];
+    };
+  };
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  programs = {
+    # Create /etc/zshrc that loads the nix-darwin environment.
+    zsh.enable = true; # default shell on catalina
+  };
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  system = {
+    # Set Git commit hash for darwin-version.
+    configurationRevision = self.rev or self.dirtyRev or null;
+
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    stateVersion = 4;
   };
 }
