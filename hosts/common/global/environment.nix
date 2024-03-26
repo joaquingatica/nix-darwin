7c32abeca7aec13508848df0484b469a6a12f9cf
Joaquin Gatica <joaquin@anagram.dev>
2024-03-26 18:53:35 -0300

    fix: update `NIX_NPM_TOKENS` env with proper quote escape

--- hosts/common/global/environment.nix
+++ hosts/common/global/environment.nix
@@ -5,5 +5,6 @@
 
   # Decrypt at eval time - useful for NIX_NPM_TOKENS
   # ref: https://elvishjerricco.github.io/2018/06/24/secure-declarative-key-management.html
+  # NOTE: quotes inside the a JSON string such as `NIX_NPM_TOKENS` must be escaped with triple backslashes `\\\`
   nix.envVars = builtins.fromJSON (builtins.extraBuiltins.decrypt "hosts/common/global/secrets/nix-env-vars.json");
 }
